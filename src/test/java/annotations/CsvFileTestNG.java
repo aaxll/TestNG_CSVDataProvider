691ab1b7ae2a5ec2eb8c3c6fb2ec700401bfe5f2
Даукша Александр Юрьевич (4053442) <adauksha@dev.vtb.ru>
2022-08-01 16:31:51 +0300

    add annotation

---
+++ src/test/java/annotations/CsvFileTestNG.java
@@ -0,0 +1,16 @@
+package annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CsvFileTestNG {
+
+  String path() default "";
+
+  int numLinesToSkip() default 0;
+
+}
